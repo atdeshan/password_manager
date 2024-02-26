42343ebc497c4a2fc4721662c795d837c7627c85
Tharindu Deshan <tharindu@Tharindus-MacBook-Pro.local>
2024-02-26 13:01:29 +0530

    basic functions are done

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from tkinter import *
+import random
 def add_password():
-    pass
+    with open("password.txt", mode="a") as file:
+        file.write(f"{web_text_field.get()} | {username_field.get()} | {password_field.get()}\n")
 
 def ganarate_password():
-    pass
-
-
-
+    charactors = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
+    password = ""
+    for _ in range(16):
+        password += random.choice(charactors)
+    password_field.delete(0,END)
+    password_field.insert(0,password)
+    
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -21,15 +26,20 @@ password_lable = Label(window, text="Password", font=("Arial", 16))
 website_lable.grid(row=1, column=0, padx=(20,0))
 emil_lable.grid(row=2, column=0,padx=(20,0))
 password_lable.grid(row=3, column=0,padx=(20,0))
-web_text_field = Entry(window, font=("Arial", 16),width=35)
-username_field = Entry(window, font=("Arial", 16),width=35)
+
+# entry
+web_text_field = Entry(window, font=("Arial", 16),width=38)
+web_text_field.focus()
+username_field = Entry(window, font=("Arial", 16),width=38)
 password_field = Entry(window, font=("Arial", 16),width=21)
+
+
 username_field.grid(row=2, column=1, columnspan=2,padx=(10,10))
 password_field.grid(row=3, column=1,padx=(10,0))
-password_genaration_button = Button(window, text="Generate Password", font=("Arial", 10),width=14,command=ganarate_password)
+password_genaration_button = Button(window, text="Generate Password", font=("Arial", 14),width=14,command=ganarate_password)
 password_genaration_button.grid(row=3, column=2,padx=(0,10))
 web_text_field.grid(row=1, column=1, columnspan=2,padx=(10,10))
-add_button = Button(window, text="Add", font=("Arial", 10),width=36,command=add_password)
+add_button = Button(window, text="Add", font=("Arial", 14),width=35,command=add_password)
 add_button.grid(row=4,column=1,columnspan=2,pady=(0,30),padx=(0,0))
 
 window.mainloop()
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-from tkinter import *
+# from tkinter import *
+# def button_has_clicked():
+#     print(text.get())
 
-window = Tk()
-window.geometry("200x200")
-r = Label(window, bg="red",width=10, height=10)
-g = Label(window,bg = "green",width=10,height=10)
-r.grid(row=0, column=0)
-g.grid(row=1, column=1)
-window.mainloop()
+
+# window = Tk()
+# window.geometry("200x200")
+# text = Entry(window)
+# text.pack()
+# button = Button(text="Click Me",command=button_has_clicked)
+# button.pack()
+# window.mainloop()
