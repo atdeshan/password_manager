884bc0200063e68e1e4dbee8c5b0c319e797fe06
Tharindu Deshan <tharindu@Tharindus-MacBook-Pro.local>
2024-02-27 23:13:19 +0530

    serch option added

--- main.py
+++ main.py
@@ -34,7 +34,16 @@ def ganarate_password():
     password_field.clipboard_clear()
     password_field.clipboard_append(password)
 
-
+def search():
+    try:
+        with open("password.json","r") as file:
+            jason_file = json.load(file)
+            password_field.delete(0,END)
+            password_field.insert(0,jason_file[web_text_field.get()]["password"])
+            password_field.clipboard_clear()
+            password_field.clipboard_append(jason_file[web_text_field.get()]["password"])
+    except:
+        massagebox.showerror("Oops","NO ENTITY FOUNDED")
 
 
 window = Tk()
@@ -53,7 +62,7 @@ emil_lable.grid(row=2, column=0,padx=(20,0))
 password_lable.grid(row=3, column=0,padx=(20,0))
 
 # entry
-web_text_field = Entry(window, font=("Arial", 16),width=38)
+web_text_field = Entry(window, font=("Arial", 16),width=21)
 web_text_field.focus()
 username_field = Entry(window, font=("Arial", 16),width=38)
 username_field.insert(0,"tharindudeshan325.com")
@@ -64,8 +73,10 @@ username_field.grid(row=2, column=1, columnspan=2,padx=(10,10))
 password_field.grid(row=3, column=1,padx=(10,0))
 password_genaration_button = Button(window, text="Generate Password", font=("Arial", 14),width=14,command=ganarate_password)
 password_genaration_button.grid(row=3, column=2,padx=(0,10))
-web_text_field.grid(row=1, column=1, columnspan=2,padx=(10,10))
+web_text_field.grid(row=1, column=1,padx=(10,0))
 add_button = Button(window, text="Add", font=("Arial", 14),width=35,command=add_password)
+search_button = Button(window,text="Search", font=("Arial", 14),width=14,command=search)
 add_button.grid(row=4,column=1,columnspan=2,pady=(0,30),padx=(0,0))
+search_button.grid(row=1,column=2,padx=(0,10))
 
 window.mainloop()
